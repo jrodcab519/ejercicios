d2e0fd33c6e36eb7a7e542656214de85357d7da8
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-06 14:28:56 +0200

    version 1.2

--- src/Gravitacion.java
+++ src/Gravitacion.java
@@ -9,17 +9,23 @@ class Gravitacion {
 
     System.out.println("Introduce la masa del cuerpo 1 (en gramos): ");
     double masa1 = entrada.nextDouble();
+    
     System.out.println("Introduce la masa del cuerpo 2 (en gramos): ");
     double masa2 = entrada.nextDouble();
+    
     System.out.println("Introduce la distancia entre ellos (en cm): ");
     double distancia = entrada.nextDouble();
 
     entrada.close();
 
-    double atraccion =  (CONSTANTE_GRAVITACIONAL * masa1 * masa2)/Math.pow(distancia,2);
+    double atraccion = getAtraccion(masa1, masa2, distancia);
 
     System.out.println(atraccion + "dinas");
+    
+  }
 
+  public static double getAtraccion(double masa1, double masa2, double distancia) {
+    return (CONSTANTE_GRAVITACIONAL * masa1 * masa2) / Math.pow(distancia, 2);
   }
 
 }
