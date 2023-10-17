090a844a3e9cd72155fe7de5fe3eb190e265add9
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-17 09:54:00 +0200

    version 1

---
+++ src/Bucle3.java
@@ -0,0 +1,22 @@
+
+
+public class Bucle3 {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i <10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("Fin del bucle for");
+
+        int j = 0;
+        while(j < 10){
+            System.out.println(j);
+            j++;
+        }
+
+        System.out.println("Fin del bucle while");
+
+    }
+}
