8b8bd0765da1cc7088866388dac3638194e29b12
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-17 09:25:39 +0200

    version 1

---
+++ src/Bucle1.java
@@ -0,0 +1,15 @@
+public class Bucle1 {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while (contador <= 10) {
+            System.out.println("contador: " + contador);
+            contador++; // contador = contador +1;
+        }
+
+        System.out.println("Fin del bucle.");
+
+    }
+}
